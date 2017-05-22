0db25eff40cddb7c1c4328fb390d63c39f0e3733
Asa Choi <asa_hp_choi@manulife.com>
2017-05-22 16:18:09 +0800

    testing

--- app.js
+++ app.js
@@ -164,7 +164,18 @@ function sendReply(recipientId, replies) {
   },
   "message":{
     "text":"Pick a color:",
-    "quick_replies":reps
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Red",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+      },
+      {
+        "content_type":"text",
+        "title":"Green",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+      }
+    ]
   }
 }
 
