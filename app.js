ad736ba893dcbb69573656cdcf3b3478e9621eb5
Asa Choi <asa_hp_choi@manulife.com>
2017-05-22 16:20:38 +0800

    testing

--- app.js
+++ app.js
@@ -168,18 +168,17 @@ function sendReply(recipientId, replies) {
         ]
       }
   }
-      console.log(JSON.stringify(template))
-
-      request({
-          url: "https://graph.facebook.com/v2.6/me/messages",
-          qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-          method: "POST",
-          json: msg
-      }, function(error, response, body) {
-          if (error) {
-              console.log("Error sending message: " + response.error);
-          }
-      });
+      
+  request({
+      url: "https://graph.facebook.com/v2.6/me/messages",
+      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+      method: "POST",
+      json: msg
+  }, function(error, response, body) {
+      if (error) {
+          console.log("Error sending message: " + response.error);
+      }
+  });
 
 }
 
